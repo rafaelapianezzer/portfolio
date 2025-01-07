062110575cbf631fc13113d503b0f4f0346954d5
Rafaela Pianezzer <121733539+rafaelapianezzer@users.noreply.github.com>
2025-01-07 10:22:01 -0300

    finalizacao

--- src/components/EmailModal.jsx
+++ src/components/EmailModal.jsx
@@ -7,11 +7,15 @@ export const EmailModal = () => {
         const email = document.getElementById('email-to-copy');
         navigator.clipboard.writeText(email.innerText)
             .then(() => {
-                setIsOpen(false); 
-            })
-            .catch((err) => {
-                console.error('Erro ao copiar o texto: ', err);
+                setIsOpen(false);
             });
+    };
+
+    const handleOverlayClick = (e) => {
+        // Verifica se o clique foi no overlay (fora do modal)
+        if (e.target.id === 'modal-overlay') {
+            setIsOpen(false);
+        }
     };
 
     return (
@@ -24,23 +28,18 @@ export const EmailModal = () => {
             </button>
 
             {isOpen && (
-                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-                    <div className="bg-neutral-50 dark:bg-neutral-900 p-4 rounded-lg text-center">
-                        <div className='flex'>
-                            <button
-                                onClick={() => setIsOpen(false)}
-                            >
-                                <i className="fa-solid fa-x text-xs py-1"></i>
-                            </button>
-                        </div>
-                        <div className='flex gap-2'>
-                            <p id="email-to-copy" className="font-bold">rafaelapianezzer@outlook.com</p>
-                            <button
-                                onClick={copyToClipboard}
-                            >
-                                <i className="fa-regular fa-copy"></i>
-                            </button>
-                        </div>
+                <div
+                    id="modal-overlay"
+                    className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
+                    onClick={handleOverlayClick}
+                >
+                    <div className="flex gap-2 justify-center items-center bg-neutral-200 dark:bg-neutral-200 p-4 rounded-lg text-center">
+                        <p id="email-to-copy" className="font-bold text-purple-900">rafaelapianezzer@outlook.com</p>
+                        <button
+                            onClick={copyToClipboard}
+                        >
+                            <i className="fa-regular fa-copy text-lg text-neutral-600"></i>
+                        </button>
                     </div>
                 </div>
             )}
